9e08fbec58b55ba007ecd40a30af22dacc595f8c
Ekaterina Petrova <ekaterinapetrova985@gmai.com>
2023-05-04 10:16:07 +0300

    Added design of cart

--- src/main/java/ru/blackmirrror/coffeeshop/models/User.java
+++ src/main/java/ru/blackmirrror/coffeeshop/models/User.java
@@ -1,5 +1,6 @@
 package ru.blackmirrror.coffeeshop.models;
 
+import org.hibernate.annotations.Columns;
 import ru.blackmirrror.coffeeshop.models.enums.Role;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
